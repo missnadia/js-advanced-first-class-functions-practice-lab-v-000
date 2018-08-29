d3867732efcbadfd6c92cc6c132a68e14e757f5b
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-29 21:02:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,3 +24,9 @@ const driversByName = function (drivers) {
     return firstDriver.name.localeCompare(secondDriver.name);
   });
 };
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, driver) {
+    return driver.revenue + total;
+  });
+};
